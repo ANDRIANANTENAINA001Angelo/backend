2f261eedc8cae23f50f4b69d6a8bdee377b3c2ea
ANDRIANANTENAINA001Angelo <morningstar25angelo@gmail.com>
2024-03-06 15:03:41 +0300

    chat-event

--- app/Models/Parcours.php
+++ app/Models/Parcours.php
@@ -4,14 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Parcours extends Model
 {
     use HasFactory;
     
     protected $fillable = [
-        'label'
+        'label',
+        "mention"
     ];
 
+    public function mention():BelongsTo{
+        return $this->belongsTo(Mention::class);
+    }
 
 }
